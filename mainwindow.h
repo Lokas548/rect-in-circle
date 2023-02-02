bdc353c574e8404f170f4057cc01879b7b134eb6
Ivan Polishchuk <90204227+Lokas548@users.noreply.github.com>
2023-02-02 18:44:05 +0200

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QRect>
+#include <QtMath>
+#include <QVector>
+#include <QPoint>
+#include <QDesktopWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void paintEvent(QPaintEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+
+private:
+    QRect screen;
+    int height,width;
+    int x,y;
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
